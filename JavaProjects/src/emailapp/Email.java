497d05e5fd338b2d41e1e7bcfed2bb175530f6f4
Dylan Medina <dlondonmedina@gmail.com>
2021-02-23 18:59:36 -0800

    Add password generator.

--- JavaProjects/src/emailapp/Email.java
+++ JavaProjects/src/emailapp/Email.java
@@ -8,6 +8,7 @@ public class Email {
 	private String password;
 	private String department;
 	private int mailboxCapacity;
+	private int passwordLengthDefault = 10;
 	private String alternateEmail;
 	
 	// Constructor to receive the first name and last name
@@ -16,6 +17,10 @@ public class Email {
 		this.lastName = lastName;
 		this.department = setDepartment();
 		System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName + " " + this.department);
+		this.password = this.randomPassword(this.passwordLengthDefault);
+		System.out.println("Your password: " + this.password);
+		// Call Method returns random password
+		
 	}
 	
 	// Ask for the department
@@ -31,7 +36,18 @@ public class Email {
 	
 	}
 	// Generate random password
-	
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#@!$%";
+		char[] password = new char[length];
+		
+		for (int i = 0; i < length; i++) {
+			int rand = (int) (Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+		}
+		
+		return new String(password);
+		
+	}
 	// Set the mailbox capacity
 	
 	// Set the alternate email
